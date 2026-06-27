e9fc9d775cf55ccd53476d5dba85c34022185edc
eomayski <emil@omayski.com>
2026-06-27 20:21:27 +0300

    Refactor flag fetching to use new API

--- src/store/flagsSlice.js
+++ src/store/flagsSlice.js
@@ -1,20 +1,19 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 
-// Национални администрации които имат собствени футболни отбори
-const SPECIAL_FLAGS = {
-    'England': 'https://flagcdn.com/w40/gb-eng.png',
-    'Scotland': 'https://flagcdn.com/w40/gb-sct.png',
-    'Wales': 'https://flagcdn.com/w40/gb-wls.png',
-    'Northern Ireland': 'https://flagcdn.com/w40/gb-nir.png',
-    'Czech Republic': 'https://flagcdn.com/w40/cz.png'
+// Fallback за UK нации, в случай че не са включени в flagcdn codes.json
+const UK_NATIONS = {
+    'England': 'gb-eng',
+    'Scotland': 'gb-sct',
+    'Wales': 'gb-wls',
+    'Northern Ireland': 'gb-nir',
 };
 
 export const fetchFlags = createAsyncThunk(
     'flags/fetchFlags',
     async (_, { rejectWithValue }) => {
         try {
-            const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags');
+            const response = await fetch('https://flagcdn.com/en/codes.json');
 
             if (!response.ok) {
                 const errorMsg = 'Failed to fetch flags data';
@@ -22,14 +21,17 @@ export const fetchFlags = createAsyncThunk(
                 return rejectWithValue(errorMsg);
             }
 
-            const data = await response.json();
+            const codes = await response.json();
 
-            const flagsDictionary = data.reduce((acc, country) => {
-                acc[country.name.common] = country.flags.png;
+            const flagsDictionary = Object.entries(codes).reduce((acc, [code, name]) => {
+                acc[name] = `https://flagcdn.com/w40/${code}.png`;
                 return acc;
             }, {});
 
-            Object.assign(flagsDictionary, SPECIAL_FLAGS);
+            Object.entries(UK_NATIONS).forEach(([name, code]) => {
+                flagsDictionary[name] = `https://flagcdn.com/w40/${code}.png`;
+            });
+
             return flagsDictionary;
         } catch (error) {
             const errorMsg = error.message || 'An unexpected error occurred';
